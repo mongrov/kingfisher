414b3a93d56b5195157755c85423fec734d8f2d5
Eyal Arasu <elarasuz@gmail.com>
2023-05-12 16:15:55 -0700

    handlebar template processing added

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 extern crate log;
 use clap::{Parser, Subcommand};
-use log::info;
+use config::Config;
+use handlebars::Handlebars;
+use log::debug;
+use std::collections::BTreeMap;
+// use std::collections::HashMap;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -12,8 +16,8 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// handlebar templates 
-    Hb { filename: String },
+    /// handlebar templates
+    Hb { config: String, template: String },
     /// Test
     Test,
 }
@@ -22,19 +26,34 @@ fn main() {
     // RUST_LOG=debug
     env_logger::init();
     let cli = Cli::parse();
-    info!("kf startup");
+    debug!("kf startup");
 
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
-        Commands::Hb { filename } => {
-            println!("File system called with {filename}");
+        Commands::Hb { config , template} => {
+            // create the handlebars registry
+            let mut handlebars = Handlebars::new();
+            let settings = Config::builder()
+                .add_source(config::File::with_name(config))
+                .build()
+                .unwrap();
+            // println!(
+            //     "{:?}",
+            //     settings
+            //         .try_deserialize::<BTreeMap<String, String>>()
+            //         .unwrap()
+            // );
+            handlebars.register_template_file("template", template).unwrap();
+            let data = settings.try_deserialize::<BTreeMap<String, String>>().unwrap();
+            println!("{}",  handlebars.render("template", &data).unwrap());
         }
         _ => {
-            // timely::example(|scope| {
-            //     (0..10)
-            //         .to_stream(scope)
-            //         .inspect(|x| println!("seen: {:?}", x));
-            // });
+            println!("Nothing here");
+            // The data type should implements `serde::Serialize`
+            // let mut data = BTreeMap::new();
+            // data.insert("world".to_string(), "世界!".to_string());
+            // println!("render {}", handlebars.render("t1", &data).unwrap());
         }
-    }}
+    }
+}
