198b0c6b0f40078a4230c73859c5b149e2a9738c
Eyal Arasu <elarasuz@gmail.com>
2023-07-29 16:30:21 -0700

    output file as param

---
+++ readme.md
@@ -0,0 +1,18 @@
+# KingFisher (kf)
+
+Fast command line tool to run template engines
+
+## Dev Workflow
+
+```bash
+cargo bump patch --git-tag
+git push --follow-tags
+```
+
+```bash
+# cargo release
+cargo publish
+
+# build a release file
+cargo build --release
+```
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use clap::{Parser, Subcommand};
 use config::Config;
 use handlebars::Handlebars;
 use log::debug;
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, fs};
 // use std::collections::HashMap;
 
 #[derive(Parser)]
@@ -17,9 +17,7 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     /// handlebar templates
-    Hb { config: String, template: String },
-    /// Test
-    Test,
+    Hb { config: String, template: String, output: String },
 }
 
 fn main() {
@@ -31,7 +29,7 @@ fn main() {
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
-        Commands::Hb { config , template} => {
+        Commands::Hb { config , template, output} => {
             // create the handlebars registry
             let mut handlebars = Handlebars::new();
             let settings = Config::builder()
@@ -46,14 +44,7 @@ fn main() {
             // );
             handlebars.register_template_file("template", template).unwrap();
             let data = settings.try_deserialize::<BTreeMap<String, String>>().unwrap();
-            println!("{}",  handlebars.render("template", &data).unwrap());
-        }
-        _ => {
-            println!("Nothing here");
-            // The data type should implements `serde::Serialize`
-            // let mut data = BTreeMap::new();
-            // data.insert("world".to_string(), "世界!".to_string());
-            // println!("render {}", handlebars.render("t1", &data).unwrap());
+            fs::write(output, handlebars.render("template", &data).unwrap()).expect("Unable to write to file");
         }
     }
 }
